a9b1f99a48011aeb0e2968a80dcbdd6cf92362ce
samkeen <sam.keen@nike.com>
2014-10-16 07:12:44 -0700

    added ui-grid to view1 http://ui-grid.info/docs/#/tutorial/101_intro

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,14 +1,23 @@
 'use strict';
 
-angular.module('busitrendClient.view1', ['ngRoute'])
+angular.module('busitrendClient.view1', ['ngRoute','ui.grid'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/view1', {
+      templateUrl: 'view1/view1.html',
+      controller: 'View1Ctrl'
+    });
+  }])
 
-.controller('View1Ctrl', [function() {
-
-}]);
+  .controller('View1Ctrl', ['$scope', '$http',
+    function ($scope, $http) {
+      $scope.listings = [];
+      $http.get('http://localhost:3000/api/business_lics?limit=10')
+        .success(function (data) {
+          $scope.listings = data;
+        })
+        .error(function (data, status, headers, config) {
+          $scope.listings = [data]
+        });
+      $scope.title = "foom";
+    }]);
